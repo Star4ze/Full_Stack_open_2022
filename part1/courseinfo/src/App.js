283391a7983a844e0e8119f549ac6d34c5c2fd07
Amindu <amindu1999@gmail.com>
2022-08-06 15:28:28 +0530

    Exercises 1.3.-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,36 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <>
-      <Header course={course} />
-      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]} />
-	  <Total exercises={[exercises1,exercises2,exercises3]}/>
+      <Header course={course.name} />
+      <Content parts= {course.parts}/>
+	  <Total exercises={course.parts}/>
     </>
   )
 }
-
+// 
 const Content = (props) =>{
   return (
     <div>
-      <Part part={props.parts[0]} exercise={props.exercises[0]} />
-	  <Part part={props.parts[1]} exercise={props.exercises[1]} />
-	  <Part part={props.parts[2]} exercise={props.exercises[2]} />
+      <Part part={props.parts[0]} />
+	  <Part part={props.parts[1]} />
+	  <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -28,7 +38,7 @@ const Content = (props) =>{
 const Part = (props) =>{
   return (
     <p>
-        {props.part} {props.exercise}
+        {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -42,9 +52,9 @@ const Header = (props) =>{
 }
 
 const Total = (props) =>{
-  return (
+	return (
     <div>
-	<p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+	<p>Number of exercises {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises}</p>
     </div>
   )
 }
